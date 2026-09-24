1d18a5fae426ca9773544baf69b47f555f1ddca4
Renata Marreros <2630582@upv.edu.mx>
2026-09-23 19:15:37 -0600

    clases de charly a github

---
+++ Src/understanding_variables.py
@@ -0,0 +1,32 @@
+# Let's try to use variables
+message = "This is my first program :), I am very happy"
+print(message)
+
+message_a_number = 22
+print(message_a_number)
+
+print(message, message_a_number)
+print(message_a_number, message)
+message = "Hola a todos"
+print(message)
+
+"""
+Los nombres de variables deben nombrarse solo con: 
+    - Letras, números y guión bajo
+    - deben comenzar con una letra, un guión bajo pero
+    no con número: message_1 (esto es correcto)
+    1_message (esto es incorrecto)
+    - no utilizar espacios 
+    - no utilizar palabras reservadas para nombrar variables
+    - utilizar inglés para nombrar variables
+"""
+
+message_1 = "Hola Amiguisimo Python"
+print(message_1)
+print(mesage_1)
+
+
+"""
+NameError: Que olvidamos establecer el valor de una variable antes de utilizarla
+o cometimos un error al ingresar el nombre de la variable.
+"""
